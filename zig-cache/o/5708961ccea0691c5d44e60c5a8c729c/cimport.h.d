f5a214d1736e3e1ee4d28018f2b50c8989fee8a6
andi ahmad <andi.saputra@salt.co.id>
2023-09-20 17:50:12 +0700

    initial commit

---
+++ zig-cache/o/5708961ccea0691c5d44e60c5a8c729c/cimport.h.d
@@ -0,0 +1,5 @@
+cimport.o: \
+  /Users/endi/project/zig/zig_raylib_series/zig-cache/o/5708961ccea0691c5d44e60c5a8c729c/cimport.h \
+  /Users/endi/project/zig/zig_raylib_series/libs/raylib/src/raylib.h \
+  /opt/homebrew/Cellar/zig/0.11.0/lib/zig/include/stdarg.h \
+  /opt/homebrew/Cellar/zig/0.11.0/lib/zig/include/stdbool.h
